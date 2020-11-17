0c45f052aae8e649410ba4bc60fef82ffcc9ebed
eandrew4738 <eandrew4738@gmail.com>
2020-11-17 11:13:13 +0530

    New Ui changes

---
+++ pharmacy.geojson.js
@@ -0,0 +1,197 @@
+var coolspot = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2015-08-08T19:03:02Z",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "node/256572195",
+      "properties": {
+        "@id": "node/256572195",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "35",
+        "addr:postcode": "00185",
+        "addr:street": "Piazza Manfredo Fanti",
+        "amenity": "coolspot",
+        "dispensing": "yes",
+        "name": "Able Mandarin Hornet"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.3524924869,
+          33.7728978434
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/256592933",
+      "properties": {
+        "@id": "node/256592933",
+        "amenity": "coolspot",
+        "created_by": "JOSM",
+        "dispensing": "yes",
+        "name": "Abundant Coral Dragon"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.3100896042,
+          33.8546577628
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/256592935",
+      "properties": {
+        "@id": "node/256592935",
+        "amenity": "coolspot",
+        "created_by": "JOSM",
+        "dispensing": "yes",
+        "name": "Abundant Garnet Millipede'"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.3999266888,
+          33.7893365452
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/256752359",
+      "properties": {
+        "@id": "node/256752359",
+        "amenity": "coolspot",
+        "dispensing": "yes",
+        "name": "Abundant Mint Cormorant"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.3327256636,          
+          33.8459886273
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/256752364",
+      "properties": {
+        "@id": "node/256752364",
+        "amenity": "coolspot",
+        "dispensing": "yes",
+        "name": "Great Cherry Poodle"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.4542947301,
+          41.5512533050
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/256752379",
+      "properties": {
+        "@id": "node/256752379",
+        "amenity": "coolspot",
+        "created_by": "JOSM",
+        "dispensing": "yes",
+        "name": "Flat Hazel Snail"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.5779330330,
+          39.2855469550
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1166982158",
+      "properties": {
+        "@id": "node/1166982158",
+        "amenity": "coolspot",
+        "name": "Clean Nylon Okapi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -97.7623357837,
+          30.2555763563
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1166983851",
+      "properties": {
+        "@id": "node/1166983851",
+        "amenity": "coolspot",
+        "name": "Wobbly Red Fish"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.9171223327,
+          40.6842367887
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1166984396",
+      "properties": {
+        "@id": "node/1166984396",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "35",
+        "addr:street": "Via dello Statuto",
+        "amenity": "coolspot",
+        "dispensing": "yes",
+        "name": "Acrobatic Walnut Camel",
+        "opening_hours": "24/7",
+        "operator": "Dr. Pierluciano Pucci"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -120.9896202720,
+          37.7131655904
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1324437899",
+      "properties": {
+        "@id": "node/1324437899",
+        "addr:city": "Roma",
+        "addr:country": "IT",
+        "addr:housenumber": "49",
+        "addr:street": "Piazza Barberini",
+        "amenity": "coolspot",
+        "fax": "+39 06 48912560",
+        "name": "Young Tangelo Seagull",
+        "opening_hours": "24/7",
+        "operator": "Dottoressa Cervini Teresa",
+        "phone": "+39 06 4825456"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.1176586922,
+          49.2793656213
+        ]
+      }
+    },
+  ]
+};
